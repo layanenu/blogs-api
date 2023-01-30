6527fb32dc5b11d669525e1bab028a81a90f2ff8
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2023-01-30 12:48:37 -0300

    Requisito 14

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,10 +1,31 @@
 const { postService } = require('../services');
+const { BlogPost, Category, User } = require('../models');
 
 const getAllPosts = async (req, res) => {
   const posts = await postService.getAllPosts();
   res.status(200).json(posts.message);
 };
 
+const getPostById = async (req, res) => {
+  try {
+    const post = await BlogPost.findOne({
+      where: { id: req.params.id },
+      include: [{ model: User, as: 'user', attributes: { exclude: ['password'] } }, {
+        model: Category, as: 'categories' }],
+    });
+
+    if (!post) {
+      return res.status(404).send({ message: 'Post does not exist' });
+    }
+
+    return res.status(200).json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Algo deu errado!' });
+  }
+};
+
 module.exports = {
   getAllPosts,
+  getPostById,
 };  
--- src/routers/post.router.js
+++ src/routers/post.router.js
@@ -6,5 +6,6 @@ const { auth, error } = require('../middlewares');
 const routers = Router();
 
 routers.get('/', auth, error, postController.getAllPosts);
+routers.get('/:id', auth, error, postController.getPostById);
 
 module.exports = routers;
